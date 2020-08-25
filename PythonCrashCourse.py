27ba955f16c4abf623ddb68a26a838d2849161c5
Mohammed Mubarak <moester9696@gmail.com>
2020-08-25 16:55:17 -0500

    Python Practice

---
+++ PythonCrashCourse.py
@@ -0,0 +1,49 @@
+# squares = [value**2 for value in range(1, 11)]
+# print(squares)
+
+# numbers = [num for num in range(1, 21)]
+# print(numbers)
+
+# numbers = [num for num in range(1, 1000001)]
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# numbers = [num for num in range(1, 21, 2)]
+# print(numbers)
+
+# multiples = [num for num in range(3, 31, 3)]
+# print(multiples)
+
+# cubes = [cube**3 for cube in range(1, 11)]
+# print(cubes)
+
+# numbers = [num for num in range(1, 10)]
+# print(f'The first three items in the list are: {numbers[0:3]}')
+
+# numbers = [num for num in range(1, 10)]
+# print(f'The three items from the middle of the list are: {numbers[3:6]}')
+
+# numbers = [num for num in range(1, 10)]
+# print(f'The last three items from the list are: {numbers[6:9]}')
+
+# pizzas = ['cheese', 'veggie', 'buffalo']
+# copy_pizzas = pizzas[:]
+
+# print(pizzas)
+# print(copy_pizzas)
+
+# pizzas.append('pepperoni')
+# copy_pizzas.append('barbecue')
+
+# print(pizzas)
+# print(copy_pizzas)
+
+
+# foods = ('burger', 'hot dog', 'nachos', 'steak', 'soup')
+# new_foods = ('pie', 'hot dog', 'nachos', 'steak', 'pizza')
+
+# for food in new_foods:
+#     print(food,end=' ')
+
+
